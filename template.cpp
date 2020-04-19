9fe749822efc66ee82c0eb5ed00b2a6225adfb96
Fabio Marchesi <39414882+Fabio11111999@users.noreply.github.com>
2020-04-19 15:39:13 +0200

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -2,7 +2,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// Begin Template IO/Utility
+// Begin Template
+
+//I/O
 #define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
 void dbg_out(){
 	cerr<<endl;
@@ -30,13 +32,50 @@ template<typename T, typename U> istream& operator>>(istream &is, pair<T,U> &p){
 	is>>p.first>>p.second;
 	return is;
 }
+
+//Utility 
 template<typename T> void minself(T &a, T &b){
 	a=min(a,b);
 }
 template<typename T> void maxself(T &a, T &b){
 	a=max(a,b);
 }
-// End Template IO/Utility
+
+// Exp/Mod
+int fastexp(long long base, long long esp, int mod){
+	long long res=1LL;
+	base%=mod;
+	while(esp){
+		if(esp%2)
+			res=(res*base)%mod;
+		esp/=2;
+		base=(base*base)%mod;
+	}
+	return res;
+}
+int inv(int n, int mod){
+	return fastexp(n,mod-2,mod);
+}
+vector<long long> __fact;
+void fill_fact(int up_to, int mod){
+	__fact.resize(up_to+1);
+	__fact[0]=1LL;
+	for(int i=1;i<=up_to;i++)
+		__fact[i]=(__fact[i-1]*i)%mod;
+}
+int bin(int n, int k, int mod){
+	if(n>(int)__fact.size() || k>(int)__fact.size())
+		fill_fact(max(n,k),mod);
+	long long den=(__fact[k]*__fact[n-k])%mod;
+	return (__fact[n]*inv(den,mod))%mod;
+}
+
+// End Template 
+
+
+
+
 int main(){
 	cin.tie(0),cin.sync_with_stdio(0),cout.tie(0),cout.sync_with_stdio(0);
+
 }
